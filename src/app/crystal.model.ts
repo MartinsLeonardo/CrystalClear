119fd4687354f8c11e94b0bf47a5f82a80e3bd63
Leonardo Martins <leodms789@gmail.com>
2018-03-22 21:04:18 -0300

    Listando Crystal Family

---
+++ src/app/crystal.model.ts
@@ -0,0 +1,12 @@
+export class Crystal {
+    type: string;
+    name: string;
+    risk: string;
+
+    constructor(type: string, name: string, risk: string) {
+        this.type = type;
+        this.name = name;
+        this.risk = risk;
+    }
+  }
+  
