f46bd6ef12b906ff6bce51c427f1a7490d147afe
Leonardo Martins <leodms789@gmail.com>
2018-05-07 22:39:39 -0300

    Desenho das páginas

--- src/app/familia/crystal.model.ts
+++ src/app/familia/crystal.model.ts
@@ -2,11 +2,13 @@ export class Crystal {
     type: string;
     name: string;
     risk: string;
+    describe: string;
 
-    constructor(type: string, name: string, risk: string) {
+    constructor(type: string, name: string, risk: string, describe: string) {
         this.type = type;
         this.name = name;
         this.risk = risk;
+        this.describe = describe;
     }
   }
   
