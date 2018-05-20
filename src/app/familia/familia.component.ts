5356d75bd97d83d557a020c0e37d5dad7d05ce4f
Leonardo Martins <leodms789@gmail.com>
2018-05-20 19:03:15 -0300

    familia

--- src/app/familia/familia.component.ts
+++ src/app/familia/familia.component.ts
@@ -10,29 +10,29 @@ export class FamiliaComponent implements OnInit {
 
   crystals = [
     new Crystal('Clear','L6','Life (L)','descrição','../../assets/img/familia/clear.png'),
-//    new Crystal('Clear','E6','Essential Money (E)','descrição'),
-  //  new Crystal('Clear','D6','Discretionary Money (D)',''),
-    //new Crystal('Clear','C6','Comfort (C)',''),
+    new Crystal('Clear','E6','Essential Money (E)','descrição','../../assets/img/familia/clear.png'),
+    new Crystal('Clear','D6','Discretionary Money (D)','','../../assets/img/familia/clear.png'),
+    new Crystal('Clear','C6','Comfort (C)','','../../assets/img/familia/clear.png'),
 
-    //    new Crystal('Yellow','L20','Life (L)',''),
-  //  new Crystal('Yellow','E20','Essential Money (E)',''),
-    //new Crystal('Yellow','D20','Discretionary Money (D)',''),
-//    new Crystal('Yellow','C20','Comfort (C)',''),
+    new Crystal('Yellow','L20','Life (L)','','../../assets/img/familia/yellow.png'),
+    new Crystal('Yellow','E20','Essential Money (E)','','../../assets/img/familia/yellow.png'),
+    new Crystal('Yellow','D20','Discretionary Money (D)','','../../assets/img/familia/yellow.png'),
+    new Crystal('Yellow','C20','Comfort (C)','','../../assets/img/familia/yellow.png'),
 
-//  new Crystal('Orange','L40','Life (L)',''),
-    //new Crystal('Orange','E40','Essential Money (E)',''),
-//    new Crystal('Orange','D40','Discretionary Money (D)',''),
-  //  new Crystal('Orange','C40','Comfort (C)',''),
+    new Crystal('Orange','L40','Life (L)','','../../assets/img/familia/orange.png'),
+    new Crystal('Orange','E40','Essential Money (E)','','../../assets/img/familia/orange.png'),
+    new Crystal('Orange','D40','Discretionary Money (D)','','../../assets/img/familia/orange.png'),
+    new Crystal('Orange','C40','Comfort (C)','','../../assets/img/familia/orange.png'),
 
-  //new Crystal('Red','L80','Life (L)',''),
-//    new Crystal('Red','E80','Essential Money (E)',''),
-  //  new Crystal('Red','D80','Discretionary Money (D)',''),
-    //new Crystal('Red','C80','Comfort (C)',''),
+    new Crystal('Red','L80','Life (L)','','../../assets/img/familia/red.png'),
+    new Crystal('Red','E80','Essential Money (E)','','../../assets/img/familia/red.png'),
+    new Crystal('Red','D80','Discretionary Money (D)','','../../assets/img/familia/red.png'),
+    new Crystal('Red','C80','Comfort (C)','','../../assets/img/familia/red.png'),
 
-    //    new Crystal('Maroon','L200','Life (L)',''),
-  //  new Crystal('Maroon','E200','Essential Money (E)',''),
-    //new Crystal('Maroon','D200','Discretionary Money (D)',''),
-//    new Crystal('Maroon','C200','Comfort (C)','')
+    new Crystal('Maroon','L200','Life (L)','','../../assets/img/familia/maroon.png'),
+    new Crystal('Maroon','E200','Essential Money (E)','','../../assets/img/familia/maroon.png'),
+    new Crystal('Maroon','D200','Discretionary Money (D)','','../../assets/img/familia/maroon.png'),
+    new Crystal('Maroon','C200','Comfort (C)','','../../assets/img/familia/maroon.png')
   ]
 
   constructor() { }
