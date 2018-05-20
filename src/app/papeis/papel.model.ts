e4e3991c0e710bf75885f2e57cf0e38f040d195b
Leonardo Martins <leodms789@gmail.com>
2018-05-20 00:32:08 -0300

    Papeis Alter

--- src/app/papeis/papel.model.ts
+++ src/app/papeis/papel.model.ts
@@ -1,10 +1,12 @@
 export class Papel {
     type: string;
     describe: string;
+    img: string;
 
-    constructor(type: string, describe: string) {
+    constructor(type: string, describe: string, img:string) {
         this.type = type;
         this.describe = describe;
+        this.img = img;
     }
   }
   
