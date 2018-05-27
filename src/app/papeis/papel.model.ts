b35506e968104443b546c55e6ebb5e6ffc8bf5c7
Leonardo Martins <leodms789@gmail.com>
2018-05-27 20:18:22 -0300

    Papéis Finalizados

--- src/app/papeis/papel.model.ts
+++ src/app/papeis/papel.model.ts
@@ -1,12 +1,14 @@
 export class Papel {
+    name: string;
     type: string;
     describe: string;
     img: string;
 
-    constructor(type: string, describe: string, img:string) {
+    constructor(type: string, name:string, describe: string, img:string) {
         this.type = type;
         this.describe = describe;
         this.img = img;
+        this.name = name;
     }
   }
   
